3931bed9ea463ca3eb61d0d00806ee06a6cf443f
Akipe <pro.julien@milletre.fr>
2017-10-04 21:22:04 +0200

    Add todo list for Sylpheed

--- automatic/sylpheed/update.ps1
+++ automatic/sylpheed/update.ps1
@@ -1,5 +1,9 @@
 import-module au
 
+# ToDo :
+#   - display version with 3 numbers all the time (example : v3.6 -> 3.6.0)
+#   - get latest changelog from https://sylpheed.sraoss.jp/en/news.html
+#   - change projectSourceUrl when update (example for v3.6.0 : https://sylpheeectSourcd.sraoss.jp/sylpheed/v3.6/sylpheed-3.6.0.tar.bz2)
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
