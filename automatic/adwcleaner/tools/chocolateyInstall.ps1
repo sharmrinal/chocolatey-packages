711902e8008e680a91bbc71e8f1305e1190ae474
Akipe <pro.julien@milletre.fr>
2017-10-12 13:20:12 +0200

    add adwcleaner

---
+++ automatic/adwcleaner/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName  = 'sylpheed'
+$urlToken     = 'https://toolslib.net/downloads/finish/1-adwcleaner/1199/'
+$FileFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\adwcleaner.exe"
+$checksum     = '52856ec13bcc140b72755fea95eb8966ed64ad03bf6d1c3d20e139e1829264a5'
+$checksumType = 'sha256'
+
+# Get download link from token link
+$page_token_source        = Invoke-WebRequest -Uri $urlToken
+$url                      = ($page_token_source.Links | Where-Object href -match "^https://download.toolslib.net/download/file/" | Select-Object -First 1).href
+
+$packageArgs = @{
+  packageName   = $packageName
+  url           = $url
+  FileFullPath  = $FileFullPath
+  checksum      = $checksum
+  checksumType  = $checksumType
+}
+
+Get-ChocolateyWebFile @packageArgs
---
+++ automatic/adwcleaner/update.ps1
@@ -0,0 +1,40 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]urlToken\s*=\s*)('.*')"       = "`$1'$($Latest.URLToken)'"
+            "(^[$]checksum\s*=\s*)('.*')"  = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+
+    $url_repo = "https://toolslib.net/downloads/viewdownload/1-adwcleaner/files/?t=release"
+    $page_repo_source = Invoke-WebRequest -Uri $url_repo
+    $html_element_link = $page_repo_source.Links | Where-Object href -match "^/downloads/viewdownload/1-adwcleaner/files/\d+" | Select-Object -First 1
+    
+    $id_latest_version = $html_element_link.href -split '/' | ? {$_} | Select-Object -Last 1
+
+    $url_download_page_token = "https://toolslib.net/downloads/finish/1-adwcleaner/" + $id_latest_version + "/"
+    $page_token_source = Invoke-WebRequest -Uri $url_download_page_token
+
+    $url_download = ($page_token_source.Links | Where-Object href -match "^https://download.toolslib.net/download/file/" | Select-Object -First 1).href
+
+    $title_with_version = $page_token_source.ParsedHtml.getElementsByTagName("h1")[0].innerHTML
+    $version = ($title_with_version.Split(" ") | Where-Object {$_} | Select-Object -last 1) -replace '[()]',''
+
+    $ChecksumType   = 'sha256'
+
+    $Latest = @{
+        Version         = $version;
+        URLToken        = $url_download_page_token
+        URL32           = $url_download
+        ChecksumType32  = $ChecksumType;
+    }
+
+    return $Latest
+}
+
+update -NoCheckUrl
