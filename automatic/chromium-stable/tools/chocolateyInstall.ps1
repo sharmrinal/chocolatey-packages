ac5bc0b3c34df27d6da38c58806cd4ce8490d702
Akipe <pro.julien@milletre.fr>
2017-10-16 04:05:46 +0200

    little code change

--- automatic/chromium-stable/tools/chocolateyInstall.ps1
+++ automatic/chromium-stable/tools/chocolateyInstall.ps1
@@ -1,21 +1,21 @@
-﻿$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop';
 
-$packageName    = 'chromium-stable'
-$fileType       = 'exe'
-$url            = 'https://github.com/henrypp/chromium/releases/download/v61.0.3163.100-r488528-win32/chromium-sync.exe'
-$url64          = 'https://github.com/henrypp/chromium/releases/download/v61.0.3163.100-r488528-win64/chromium-sync.exe'
-$softwareName   = 'chromium-stable*'
-$checksum       = '536d196181b0ba4ffd33bb6bfff70d88ca075f77bdc3b171a01fcddfb2cad1a6'
-$checksum64     = 'de8c71e96d7b995bc8e8334160f62d713790d27d00a94856512b47abf9a417cd'
-$checksumTypeAll= 'sha256'
+$packageName    = 'chromium-stable';
+$fileType       = 'exe';
+$url            = 'https://github.com/henrypp/chromium/releases/download/v61.0.3163.100-r488528-win32/chromium-sync.exe';
+$url64          = 'https://github.com/henrypp/chromium/releases/download/v61.0.3163.100-r488528-win64/chromium-sync.exe';
+$softwareName   = 'chromium-stable*';
+$checksum       = '536d196181b0ba4ffd33bb6bfff70d88ca075f77bdc3b171a01fcddfb2cad1a6';
+$checksum64     = 'de8c71e96d7b995bc8e8334160f62d713790d27d00a94856512b47abf9a417cd';
+$checksumTypeAll= 'sha256';
 
-$registryPathChromiumAsUser = 'hkcu:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium'
-$isChromiumInstallAsUser    = Test-Path $registryPathChromiumAsUser
+$registryPathChromiumAsUser = 'hkcu:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium';
+$isChromiumInstallAsUser    = Test-Path $registryPathChromiumAsUser;
  
 if ($isChromiumInstallAsUser) {
-  $silentArgs = '--do-not-launch-chrome'
+  $silentArgs = '--do-not-launch-chrome';
 } else {
-  $silentArgs = '--system-level --do-not-launch-chrome'
+  $silentArgs = '--system-level --do-not-launch-chrome';
 }
 
 $packageArgs = @{
@@ -30,6 +30,6 @@ $packageArgs = @{
   checksumType64= $checksumTypeAll
   silentArgs    = $silentArgs
   validExitCodes= @(0)
-}
+};
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyPackage @packageArgs;
--- automatic/chromium-stable/tools/chocolateyUninstall.ps1
+++ automatic/chromium-stable/tools/chocolateyUninstall.ps1
@@ -1,19 +1,19 @@
-$uninstallCommand   = (Get-UninstallRegistryKey -SoftwareName "chromium").UninstallString
-$isAdminInstall     = ($uninstallCommand -match "--system-level")
-$silentArgs         = '--uninstall --force-uninstall'
+$uninstallCommand   = (Get-UninstallRegistryKey -SoftwareName "chromium").UninstallString;
+$isAdminInstall     = ($uninstallCommand -match "--system-level");
+$silentArgs         = '--uninstall --force-uninstall';
 
 if ($isAdminInstall) {
     $silentArgs = $silentArgs + ' --system-level';
 }
 
-$uninstallerFile = $uninstallCommand -split '"' | Where-Object {$_} | Select-Object -First 1
+$uninstallerFile = $uninstallCommand -split '"' | Where-Object {$_} | Select-Object -First 1;
 
 $packageArgs = @{
-    PackageName     = 'Chromium'
+    PackageName     = 'chromium-stable'
     FileType        = 'exe'
     SilentArgs      = $silentArgs
     validExitCodes  = @(0,19,21)
     File            = $uninstallerFile
-}
+};
 
-Uninstall-ChocolateyPackage @packageArgs
+Uninstall-ChocolateyPackage @packageArgs;
