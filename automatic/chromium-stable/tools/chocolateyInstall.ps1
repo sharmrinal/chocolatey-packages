7e46376fb22b5fec1066595e4c4c63e4b01cb187
Akipe <pro.julien@milletre.fr>
2017-10-18 18:09:14 +0000

    AU: 1 updated - chromium-stable [skip ci] https://gist.github.com/00330331551bc3703af5c89a1d82bfeb/2b87bb1b45a0ce23be6c11597afef3dc8197b6f5

--- automatic/chromium-stable/tools/chocolateyInstall.ps1
+++ automatic/chromium-stable/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@
 
 $packageName    = 'chromium-stable';
 $fileType       = 'exe';
-$url            = 'https://github.com/henrypp/chromium/releases/download/v61.0.3163.100-r488528-win32/chromium-sync.exe';
-$url64          = 'https://github.com/henrypp/chromium/releases/download/v61.0.3163.100-r488528-win64/chromium-sync.exe';
+$url            = 'https://github.com/henrypp/chromium/releases/download/v62.0.3202.62-r499098-win32/chromium-sync.exe';
+$url64          = 'https://github.com/henrypp/chromium/releases/download/v62.0.3202.62-r499098-win64/chromium-sync.exe';
 $softwareName   = 'chromium-stable*';
-$checksum       = '536d196181b0ba4ffd33bb6bfff70d88ca075f77bdc3b171a01fcddfb2cad1a6';
-$checksum64     = 'de8c71e96d7b995bc8e8334160f62d713790d27d00a94856512b47abf9a417cd';
+$checksum       = 'b79649e84b5efea76fbf6a72b9b03f2c2b69b6aff29317e6400505e65015b81c';
+$checksum64     = '2b4766a2c316d63a2c531097ba8b9144a945d5a465d29bbdc79ac60b76128416';
 $checksumTypeAll= 'sha256';
 
 $registryPathChromiumAsUser = 'hkcu:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium';
