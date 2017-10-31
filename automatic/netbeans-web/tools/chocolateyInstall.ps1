ecf5b79b5cbe7366c76f061b2ea867ef0b294944
Akipe <pro.julien@milletre.fr>
2017-11-01 00:00:59 +0100

    add x64 arch for netbeans-web

--- automatic/netbeans-web/tools/chocolateyInstall.ps1
+++ automatic/netbeans-web/tools/chocolateyInstall.ps1
@@ -4,14 +4,16 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageName	= 'netbeans'
 $fileType		= 'exe'
-$filePath		= Get-Item $toolsDir\*.exe
+$32filePath		= Get-Item "$toolsDir\*_x32.exe"
+$64filePath		= Get-Item "$toolsDir\*_x64.exe"
 $softwareName	= 'NetBeans*'
 $silentArgs		= '--silent'
 
 $packageArgs = @{
 	PackageName    	= $packageName
 	FileType       	= $fileType
-	File           	= $filePath
+	File           	= $32filePath
+	File64          = $64filePath
 	SoftwareName	= $softwareName
 	SilentArgs		= $silentArgs
 	ValidExitCodes	= @(0)
