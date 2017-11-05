74da266b250a27dabc7ef11d73b0f552f25925c2
Akipe <pro.julien@milletre.fr>
2017-11-05 01:56:04 +0100

    add netbeans-cpp (replace netbeans-c++)

--- automatic/netbeans-cpp/tools/chocolateyInstall.ps1
+++ automatic/netbeans-cpp/tools/chocolateyInstall.ps1
@@ -2,16 +2,18 @@
 
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
-$packageName	= 'netbeans'
+$packageName	= 'netbeans-cpp'
 $fileType		= 'exe'
-$filePath		= Get-Item $toolsDir\*.exe
+$32filePath		= Get-Item "$toolsDir\*_x32.exe"
+$64filePath		= Get-Item "$toolsDir\*_x64.exe"
 $softwareName	= 'NetBeans*'
 $silentArgs		= '--silent'
 
 $packageArgs = @{
 	PackageName    	= $packageName
 	FileType       	= $fileType
-	File           	= $filePath
+	File           	= $32filePath
+	File64          = $64filePath
 	SoftwareName	= $softwareName
 	SilentArgs		= $silentArgs
 	ValidExitCodes	= @(0)
---
+++ automatic/netbeans-cpp/tools/chocolateyUninstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+# Uninstall Netbeans
+$regeditItem 		= 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\nbi-nb-base*'
+$UninstallProperty	= Get-ItemProperty -Path $regeditItem -Name 'UninstallString'
+
+$packageName  	= 'netbeans-cpp'
+$fileType     	= 'EXE'
+$silentArgs   	= '--silent'
+$uninstallPath	= $UninstallProperty.UninstallString
+
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $fileType -SilentArgs $silentArgs -File $uninstallPath -ValidExitCodes @(0)
