3cfa35702fc91585f7978a6fd599fc07313e0643
sharmrinal <47933616+sharmrinal@users.noreply.github.com>
2020-08-21 13:56:53 -0500

    Update chocolateyInstall.ps1

--- automatic/opentrack/tools/chocolateyInstall.ps1
+++ automatic/opentrack/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName    = 'opentrack';
 $fileType       = 'exe';
-$url            = 'https://github.com/opentrack/opentrack/releases/download/opentrack-2.3.9/opentrack-2.3.9-win32-setup.exe';
+$url            = 'https://github.com/opentrack/opentrack/releases/download/opentrack-2.3.12/opentrack-2.3.12-win32-setup.exe';
 $softwareName   = 'opentrack*';
 $checksum       = '544fb87b5742083e41346d139ca7b3d0c0427c93ff99c304bd1c87a80c99c88c';
 $checksumTypeAll= 'sha256';
